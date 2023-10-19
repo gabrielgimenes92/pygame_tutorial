7e8154051f4c0a400ac676a8f962648d63faed0f
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2023-10-19 12:34:35 -0700

    First commit

---
+++ pygameTutorial.py
@@ -0,0 +1,14 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))  # width and height
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all our elements
+    # update everything
+    pygame.display.update()
