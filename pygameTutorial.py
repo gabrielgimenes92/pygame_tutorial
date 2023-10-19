1667c5300ecf2c47e22b8369627d4cb2737c16f9
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2023-10-19 15:26:41 -0700

    Adds collision between player and snail and changes snail to use a rectangle

--- pygameTutorial.py
+++ pygameTutorial.py
@@ -12,13 +12,19 @@ clock = pygame.time.Clock()
 # font type, font size (none is the default)
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+# convert image to a format easier for pygame
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 # test, Anti Aliasing[Bool], color
 text_surface = test_font.render('My game', False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_x_pos = 600
+# with convert_alpha deletes the alpha values
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom=(600, 300))
+
+player_surf = pygame.image.load(
+    'graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -31,10 +37,15 @@ while True:
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
 
-    snail_x_pos -= 4
-    if snail_x_pos < -100:
-        snail_x_pos = 810
-    screen.blit(snail_surface, (snail_x_pos, 260))
+    snail_rect.x -= 4
+    if snail_rect.right < 0:
+        snail_rect.left = 800
+
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
+
+    if player_rect.colliderect(snail_rect):
+        print("fudge")
 
     pygame.display.update()
     clock.tick(60)
