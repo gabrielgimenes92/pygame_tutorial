60d936d82d4bc9e9f6da1a388b0be6a03904921a
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2023-10-19 12:49:38 -0700

    Initiates the screen with icon and name, adds first test surface

--- pygameTutorial.py
+++ pygameTutorial.py
@@ -3,12 +3,24 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))  # width and height
+pygame.display.set_caption('Runner')
+
+icon = pygame.image.load('./assets/runner_icon.jpeg')
+pygame.display.set_icon(icon)
+
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))  # width and height
+test_surface.fill('Red')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all our elements
-    # update everything
+
+    # block image transfer -> puts one surface on anothe surface
+    screen.blit(test_surface, (0, 0))
+
     pygame.display.update()
+    clock.tick(60)
