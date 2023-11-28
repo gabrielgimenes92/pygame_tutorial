d300176798cc44032903740fd51a04a7bf1e6ec0
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2023-11-28 01:47:30 -0800

    Adds sound and music

--- pygameTutorial.py
+++ pygameTutorial.py
@@ -2,7 +2,6 @@ import pygame
 from sys import exit
 from random import randint, choice
 
-
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -16,10 +15,14 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom=(80, 300))
         self.gravity = 0
         
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.2)
+        
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -20
+            self.jump_sound.play()
             
     def apply_gravity(self):
         self.gravity += 1
@@ -103,15 +106,16 @@ def player_animation():
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
-
 icon = pygame.image.load('graphics/runner_icon.jpeg')
 pygame.display.set_icon(icon)
-
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 game_active = False
 start_time = 0
 score = 0
+bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music.set_volume(0.1)
+bg_music.play(loops = -1)
 
 player = pygame.sprite.GroupSingle()
 player.add(Player())
